e12848c4290ce73b2dd97ba43783428a6bacfc32
teobgeno <teobgeno@gmail.com>
2023-10-15 16:00:24 +0300

    -

--- utils.py
+++ utils.py
@@ -3,12 +3,12 @@ openai_api_key = "<Your OpenAI API>"
 # Put your name
 key_owner = "<Name>"
 
-maze_assets_loc = "../../environment/frontend_server/static_dirs/assets"
+maze_assets_loc = "environment/static_dirs/assets"
 env_matrix = f"{maze_assets_loc}/the_ville/matrix"
 env_visuals = f"{maze_assets_loc}/the_ville/visuals"
 
-fs_storage = "../../environment/frontend_server/storage"
-fs_temp_storage = "../../environment/frontend_server/temp_storage"
+fs_storage = "environment/storage"
+fs_temp_storage = "environment/temp_storage"
 
 collision_block_id = "32125"
 
