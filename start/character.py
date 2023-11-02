fc391652b61f4ed354f1aca906a1d6d118ad2e4c
vgenopoulos <vgenopoulos@IT3>
2023-11-02 13:39:06 +0200

    -

---
+++ start/character.py
@@ -0,0 +1,10 @@
+class Character:
+    def __init__(self, name:str):
+        self.name = name
+        #age
+        #skills List of skill(class)
+        #traits List of trait(class)
+    @classmethod
+    def create(self):
+        self.name = "ok"
+        pass
