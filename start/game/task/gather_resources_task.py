5ba1a2868cd93e4abd394ed078dc6d9d549e3bdf
vgenopoulos <vgenopoulos@IT3>
2023-11-14 12:48:53 +0200

    -

---
+++ start/game/task/gather_resources_task.py
@@ -0,0 +1,15 @@
+
+class GatherResourcesTask:
+    def __init__(self, props):
+        self._id = props["id"]
+
+    @classmethod
+    def create(cls, props):
+        return cls(props)
+
+    @property
+    def id(self):
+        return self._id
+
+    def excute():
+        pass
