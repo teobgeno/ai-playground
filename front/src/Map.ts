6b734a3df5396f5d474db6a6a68f11657b8c0c96
teobgeno <teobgeno@gmail.com>
2023-12-14 19:01:41 +0200

    -

--- front/src/Map.ts
+++ front/src/Map.ts
@@ -2,30 +2,31 @@ import {
   GridEngineHeadless,
   ArrayTilemap,
   CollisionStrategy,
-} from "grid-engine"
-import { AsciiRenderer } from "./AsciiRenderer"
+} from "grid-engine";
+import { AsciiRenderer } from "./AsciiRenderer";
+
+import {
+  countInstances,
+  sortObjsByProperty
+} from "./Utils";
 
 export class Map {
-  private gridEngineHeadless: GridEngineHeadless
-  private asciiRenderer: AsciiRenderer
-  private tilemap: ArrayTilemap
-  private layers: any
-  private gameObjects: any
+  private gridEngineHeadless: GridEngineHeadless;
+  private asciiRenderer: AsciiRenderer;
+  private tilemap: ArrayTilemap;
+  private layers: any;
+  private gameObjects: any;
 
   constructor() {
-    this.gridEngineHeadless = new GridEngineHeadless()
+    this.gridEngineHeadless = new GridEngineHeadless();
     this.layers = [
-      { layer: "forestLayer", sectionCode: 2 },
-      { layer: "forestLayer2", sectionCode: 2 },
-    ]
+      { layer: "forestLayer", sectionId: 1 }
+    ];
 
-    this.gameObjects = [{ title: "tree", mapCode: 2 }]
+    this.gameObjects = [{ title: "tree", mapCode: 2 }];
   }
 
   public initMap() {
-    // A simple example tilemap created from an array.
-    // 0 = non-blocking
-    // 1 = blocking
     this.tilemap = new ArrayTilemap({
       collisions: {
         data: [
@@ -55,27 +56,43 @@ export class Map {
           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         ],
       },
-    })
+    });
     this.gridEngineHeadless.create(this.tilemap, {
       characters: [{ id: "player" }],
       characterCollisionStrategy: CollisionStrategy.BLOCK_ONE_TILE_AHEAD,
-    })
+    });
 
     this.asciiRenderer = new AsciiRenderer(
       "content",
       this.gridEngineHeadless,
       this.tilemap
-    )
-    this.asciiRenderer.render()
+    );
+    this.asciiRenderer.render();
   }
 
-  public findSection(sectionCode) {
-    let s = (this.tilemap as any).map.forestLayer.data
-    return s
+  public findNearestSection(sectionIds: Array<number>) {
+    const findSections = this.layers.filter(x=> sectionIds.includes(x.sectionId));
+    //if findSections.length > 1 more than one layer merge to one array
+    console.log(findSections)
+    if(findSections.length === 1) {
+      return (this.tilemap as any).map[findSections[0].layer].data;
+    }
+  }
+
+  public findNearestGameObject(s, objCode) {
+    let distances: any = [];
+    const instances = countInstances(s);
+    if (instances[objCode] > 1) {
+      distances = sortObjsByProperty(
+        this.calcGameObjectsDistances(s, objCode),
+        "distance"
+      );
+    }
+    return distances[0];
   }
 
   public findAroundGameObject(gameObject) {
-    let t = (this.tilemap as any).map.collisions.data
+    let t = (this.tilemap as any).map.collisions.data;
     const neighbourTiles = {
       top: { x: gameObject.x, y: gameObject.y - 1, distance: 0 },
       topLeft: { x: gameObject.x - 1, y: gameObject.y - 1, distance: 0 },
@@ -85,57 +102,44 @@ export class Map {
       bottom: { x: gameObject.x, y: gameObject.y + 1, distance: 0 },
       bottomLeft: { x: gameObject.x - 1, y: gameObject.y + 1, distance: 0 },
       bottomRight: { x: gameObject.x + 1, y: gameObject.y + 1, distance: 0 },
-    }
+    };
 
-    let freeTiles: any = []
+    let freeTiles: any = [];
     for (const [key, value] of Object.entries(neighbourTiles)) {
-      t.hasOwnProperty(value.x)
+      t.hasOwnProperty(value.x);
       if (t.hasOwnProperty(value.y) && t[value.y].hasOwnProperty(value.x)) {
         if (t[value.y][value.x] !== 1) {
-          value.distance = this.manhattanDist(0, 0, value.y, value.x)
-          freeTiles.push(value)
+          value.distance = this.manhattanDist(0, 0, value.y, value.x);
+          freeTiles.push(value);
         }
       }
     }
 
-    console.log(freeTiles)
-    return freeTiles[2]
-    return { x: gameObject.x, y: gameObject.y - 1 }
-  }
-
-  public findNearestGameObject(s, objCode) {
-    let distances: any = []
-    const instances = this.countGameObjectInstances(s)
-    if (instances[objCode] > 1) {
-      distances = this.sortObjsByProperty(
-        this.calcGameObjectsDistances(s, objCode),
-        "distance"
-      )
-    }
-    return distances[0]
+    console.log(freeTiles);
+    return freeTiles[2];
+    return { x: gameObject.x, y: gameObject.y - 1 };
   }
 
   private calcGameObjectsDistances(s, objCode) {
-    let distances: any = []
+    let distances: any = [];
     for (let i = 0; i < s.length; i++) {
-      let row = s[i]
+      let row = s[i];
       for (let j = 0; j < row.length; j++) {
         if (row[j] == 2) {
           distances.push({
             x: j,
             y: i,
             distance: this.manhattanDist(0, 0, i, j),
-          })
+          });
         }
       }
     }
-    return distances
+    return distances;
   }
 
   private manhattanDist(x1, y1, x2, y2) {
     //https://gamedev.stackexchange.com/questions/31546/find-nearest-tile-of-type-x
-    let dist = Math.abs(x2 - x1) + Math.abs(y2 - y1)
-    return dist
+    let dist = Math.abs(x2 - x1) + Math.abs(y2 - y1);
+    return dist;
   }
-
 }
--- front/src/main.ts
+++ front/src/main.ts
@@ -1,7 +1,5 @@
 import {
   GridEngineHeadless,
-  ArrayTilemap,
-  CollisionStrategy,
 } from "grid-engine"
 import { AsciiRenderer } from "./AsciiRenderer"
 import { NoIdeaClient } from "./no-idea-client"
@@ -13,63 +11,15 @@ import { Character } from "./Character"
 export class App {
   private gridEngineHeadless: GridEngineHeadless
   private asciiRenderer: AsciiRenderer
-  private tilemap: ArrayTilemap
   private noIdeaClient: NoIdeaClient
   private moveInterval: any
 
   constructor() {
-    this.gridEngineHeadless = new GridEngineHeadless()
     this.noIdeaClient = new NoIdeaClient({
       BASE: "http://localhost:8000",
     })
   }
 
-  public initMap() {
-    // A simple example tilemap created from an array.
-    // 0 = non-blocking
-    // 1 = blocking
-    this.tilemap = new ArrayTilemap({
-      collisions: {
-        data: [
-          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        ],
-      },
-      forestLayer: {
-        data: [
-          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [2, 0, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        ],
-      },
-    })
-    this.gridEngineHeadless.create(this.tilemap, {
-      characters: [{ id: "player" }],
-      characterCollisionStrategy: CollisionStrategy.BLOCK_ONE_TILE_AHEAD,
-    })
-
-    this.asciiRenderer = new AsciiRenderer(
-      "content",
-      this.gridEngineHeadless,
-      this.tilemap
-    )
-    this.asciiRenderer.render()
-  }
 
   public moveCharacter(targetPos) {
     //const targetPos = { x: 1, y: 8 }
@@ -108,124 +58,33 @@ export class App {
   }
 
 
-  public createTask() {
-    const t = this.noIdeaClient.default.testTestGet();
+  public async createTask() {
+    const t:any = await this.noIdeaClient.default.testTestGet();
     let task = new Task();
     let map = new Map();
+    map.initMap();
     let character = new Character();
 
-    task.addAction(new Action(map.findSection,[1]));
+    task.addAction(new Action(map.findNearestSection,[t.params.sections]));
     task.addAction(new Action(map.findNearestGameObject,['[0]',2]));
     task.addAction(new Action(map.findAroundGameObject,['[1]']));
     task.addAction(new Action(character.move,['[1]']));
+    console.log(t)
+    console.log(map.findNearestSection(t.params.sections))
     //task.addAction(new Action(map.findNearestGameObject(1)))
     
-    console.log(t)
-  }
-
-  public getApiData() {
-    const t = this.noIdeaClient.default.testTestGet()
-    
+   
   }
 
   public locateAction() {
-    const sectionTiles = this.findSection(1)
-    const gameObject = this.findNearestGameObject(sectionTiles, 2)
-    const nearGameObject = this.findAroundGameObject(gameObject)
-    this.moveCharacter(nearGameObject)
-    console.log(gameObject)
-    console.log(nearGameObject)
-  }
-
-  private findSection(sectionCode) {
-    let s = (this.tilemap as any).map.forestLayer.data
-    return s
-  }
-
-  private findAroundGameObject(gameObject) {
-    let t = (this.tilemap as any).map.collisions.data
-    const neighbourTiles = {
-      top: { x: gameObject.x, y: gameObject.y - 1, distance: 0},
-      topLeft: { x: gameObject.x - 1, y: gameObject.y - 1, distance: 0},
-      topRight: { x: gameObject.x + 1, y: gameObject.y - 1, distance: 0},
-      left: { x: gameObject.x - 1, y: gameObject.y, distance: 0},
-      right: { x: gameObject.x + 1, y: gameObject.y, distance: 0},
-      bottom: { x: gameObject.x, y: gameObject.y + 1, distance: 0},
-      bottomLeft: { x: gameObject.x - 1, y: gameObject.y + 1, distance: 0},
-      bottomRight: { x: gameObject.x + 1, y: gameObject.y + 1, distance: 0},
-    }
-
-
-    let freeTiles:any = [];
-    for (const [key, value] of Object.entries(neighbourTiles)) {
-      t.hasOwnProperty(value.x)
-      if (t.hasOwnProperty(value.y) && t[value.y].hasOwnProperty(value.x)) {
-        if(t[value.y][value.x] !== 1){
-          value.distance = this.manhattanDist(0, 0, value.y, value.x);
-          freeTiles.push(value);
-        }
-      }
-    }
-
-    
-    console.log(freeTiles)
-    return freeTiles[2]
-    return { x: gameObject.x, y: gameObject.y - 1 }
-  }
-
-  private findNearestGameObject(s, objCode) {
-    let distances: any = []
-    const instances = this.countGameObjectInstances(s)
-    if (instances[objCode] > 1) {
-      distances = this.sortObjsByProperty(
-        this.calcGameObjectsDistances(s, objCode),
-        "distance"
-      )
-    }
-    return distances[0]
-  }
-
-  private calcGameObjectsDistances(s, objCode) {
-    let distances: any = []
-    for (let i = 0; i < s.length; i++) {
-      let row = s[i]
-      for (let j = 0; j < row.length; j++) {
-        if (row[j] == 2) {
-          distances.push({
-            x: j,
-            y: i,
-            distance: this.manhattanDist(0, 0, i, j),
-          })
-        }
-      }
-    }
-    return distances
-  }
-
-  private manhattanDist(x1, y1, x2, y2) {
-    //https://gamedev.stackexchange.com/questions/31546/find-nearest-tile-of-type-x
-    let dist = Math.abs(x2 - x1) + Math.abs(y2 - y1)
-    return dist
-  }
-
-  private countGameObjectInstances(s) {
-    return s.reduce((acc, arr) => {
-      for (const item of arr) {
-        acc[item] = acc[item] !== undefined ? acc[item] + 1 : 1
-      }
-
-      return acc
-    }, {})
-  }
-
-  private sortObjsByProperty(objs, property) {
-    return objs.sort((a, b) => a[property] - b[property])
+    // const sectionTiles = this.findSection(1)
+    // const gameObject = this.findNearestGameObject(sectionTiles, 2)
+    // const nearGameObject = this.findAroundGameObject(gameObject)
+    // this.moveCharacter(nearGameObject)
+    // console.log(gameObject)
+    // console.log(nearGameObject)
   }
 }
 
 const app = new App()
-//app.getApiData();
-app.initMap()
-//app.moveCharacter();
-//app.locateAction()
 app.createTask();
