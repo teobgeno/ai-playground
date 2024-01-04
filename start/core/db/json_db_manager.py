678d10fe21ca50f86000e7c6df69408ba2621796
vgenopoulos <vgenopoulos@IT3>
2024-01-04 15:03:42 +0200

    -

--- start/core/db/json_db_manager.py
+++ start/core/db/json_db_manager.py
@@ -1,11 +1,12 @@
 class JsonDBManager:
-    def getGameSectionsDb():
+    def getGameSectionsDb(self):
         return [{'id': 1, 'parent_id': 0, 'keyword': 'forest', 'title': 'Forest 1'},
                 {'id': 2, 'parent_id': 0, 'keyword': 'lake'},
-                {'id': 3, 'parent_id': 0, 'keyword': 'forest', 'title': 'Forest 2'}
+                {'id': 3, 'parent_id': 0, 'keyword': 'forest', 'title': 'Forest 2'},
+                {'id': 4, 'parent_id': 1, 'keyword': 'house', 'title': 'House'}
                 ]
 
-    def getGameObjects():
+    def getGameObjects(self):
         return [{'id': 1, 'section_id': 1, 'parent_id': 0, 'keyword': 'tree'},
                 {'id': 2, 'section_id': 2, 'parent_id': 0, 'keyword': 'water'},
                 {'id': 2, 'section_id': 2, 'parent_id': 0, 'keyword': 'fish'}
