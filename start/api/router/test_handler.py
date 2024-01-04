ef2893a58f0799eade9c78630fdc895193ff9758
vgenopoulos <vgenopoulos@IT3>
2024-01-04 16:06:19 +0200

    -

--- start/api/router/test_handler.py
+++ start/api/router/test_handler.py
@@ -1,9 +1,24 @@
 from fastapi import APIRouter, Depends
 from api.context import get_db
+from pydantic import BaseModel
 from core.db.json_db_manager import JsonDBManager
 from game.deb import test_whatever
 
 router = APIRouter()
+
+
+class SectionDef(BaseModel):
+    id: int
+    parent_id: int
+    keyword: str
+    title: str
+
+
+class GameObjectDef(BaseModel):
+    id: int
+    section_id: int
+    parent_id: int
+    keyword: str
 
 
 @router.get("/")
@@ -16,11 +31,11 @@ async def test(db: JsonDBManager = Depends(get_db)):
     return test_whatever(db)
 
 
-@router.get("/getGameSections")
+@router.get("/getGameSections", response_model=list[SectionDef])
 async def getSections(db: JsonDBManager = Depends(get_db)):
     return db.getGameSections()
 
 
-@router.get("/getGameObjects")
+@router.get("/getGameObjects", response_model=list[GameObjectDef])
 async def getGameObjects(db: JsonDBManager = Depends(get_db)):
     return db.getGameObjects()
