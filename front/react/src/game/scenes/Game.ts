b9e45dea427b4f7456b4f50c446c61b9c488a135
vgenopoulos <vgenopoulos@IT3>
2024-05-16 11:59:44 +0300

    -

--- front/react/src/game/scenes/Game.ts
+++ front/react/src/game/scenes/Game.ts
@@ -12,8 +12,10 @@ export class Game extends Scene {
     gridEngine: GridEngine;
     hero: Character;
     cursors: Phaser.Types.Input.Keyboard.CursorKeys;
-    // private map!: Tilemaps.Tilemap;
+    private map!: Tilemaps.Tilemap;
     private tileset!: Tilemaps.Tileset;
+    private marker;
+    private propertiesText: Phaser.GameObjects.Text;
     // private treesLayer!: Tilemaps.TilemapLayer;
 
     constructor() {
@@ -24,6 +26,13 @@ export class Game extends Scene {
         //this.load.baseURL = 'assets/';
 
         //https://www.html5gamedevs.com/topic/31300-how-to-get-tiled-json-map-into-phaser-json-tile-map-error-cannot-read-property-2-of-undefined/
+        //https://newdocs.phaser.io/docs/3.60.0/focus/Phaser.Tilemaps.Tilemap-getTileAt
+        //https://labs.phaser.io/edit.html?src=src/tilemap\paint%20tiles.js
+        //https://labs.phaser.io/edit.html?src=src/tilemap\tile%20properties.js
+
+        //https://newdocs.phaser.io/docs/3.54.0/focus/Phaser.Physics.Matter.PointerConstraint-pointer
+        //https://labs.phaser.io/view.html?src=src/input\pointer\drag%20rectangle.js
+        //https://labs.phaser.io/edit.html?src=src/tilemap\mouse%20wheel%20zoom.js
         // MAP LOADING
         this.load.image({
             key: "tiles",
@@ -48,7 +57,7 @@ export class Game extends Scene {
         this.hero = this.add.existing(
             new Character(this, "hero", this.gridEngine)
         );
-        const map = this.initMap();
+        this.map = this.initMap();
        
         this.physics.add.existing(this.hero);
         this.hero.getBody().setCollideWorldBounds(true);
@@ -85,14 +94,15 @@ export class Game extends Scene {
                 },
             ],
         };
-        this.gridEngine.create(map, gridEngineConfig);
+        this.gridEngine.create(this.map, gridEngineConfig);
 
-        this.initCamera(map);
+        this.initCamera(this.map);
 
-        this.add.text(10, 10, "Move the mouse", {
-            font: "16px Courier",
-            color: "#00ff00",
+        this.propertiesText = this.add.text(16, 16, 'Click on a tile to view its properties.', {
+            font: '20px Arial',
+            color: '#000'
         });
+      
 
         // this.gridEngine.create(map, gridEngineConfig);
 
@@ -108,25 +118,30 @@ export class Game extends Scene {
         //     align: 'center'
         // }).setOrigin(0.5).setDepth(100);
 
-        this.input.on('pointerup', (pointer:any) => {
-            // Get the WORLD x and y position of the pointer
-            const {worldX, worldY} = pointer;
-            console.log(pointer)
-            const t = this.add.sprite(worldX, worldY, 'items', 'basic_axe');
-            t.setDepth(1)
-            // Assign the world x and y to our vector
-            // const target ={x:0,y:0};
-            // target.x = worldX;
-            // target.y = worldY;
+        // this.input.on('pointerup', (pointer:any) => {
+        //     // Get the WORLD x and y position of the pointer
+        //     const {worldX, worldY} = pointer;
+        //     console.log(pointer)
+        //     const t = this.add.sprite(worldX, worldY, 'items', 'basic_axe');
+        //     t.setDepth(1)
+        //     // Assign the world x and y to our vector
+        //     // const target ={x:0,y:0};
+        //     // target.x = worldX;
+        //     // target.y = worldY;
       
-            // // Position the arrow at our world x and y
-            // this.arrow.body.reset(worldX, worldY);
-            // this.arrow.setVisible(true);
+        //     // // Position the arrow at our world x and y
+        //     // this.arrow.body.reset(worldX, worldY);
+        //     // this.arrow.setVisible(true);
       
-            // // Start moving our cat towards the target
-            //this.setPosition(128, 256);
-            //this.scene.physics.moveToObject(this, pointer, 200);
-          });
+        //     // // Start moving our cat towards the target
+        //     //this.setPosition(128, 256);
+        //     //this.scene.physics.moveToObject(this, pointer, 200);
+        // });
+
+        this.marker = this.add.graphics();
+        this.marker.lineStyle(2, 0x000000, 1);
+        this.marker.strokeRect(0, 0, this.map.tileWidth, this.map.tileHeight);
+        
 
         EventBus.emit("current-scene-ready", this);
     }
@@ -201,6 +216,34 @@ export class Game extends Scene {
     }
 
     update(): void {
+
+        const worldPoint = this.input.activePointer.positionToCamera(this.cameras.main);
+
+        // Rounds down to nearest tile
+        const pointerTileX = this.map.worldToTileX(worldPoint.x);
+        const pointerTileY = this.map.worldToTileY(worldPoint.y);
+
+        
+
+        // Snap to tile coordinates, but in world space
+        this.marker.x = this.map.tileToWorldX(pointerTileX);
+        this.marker.y = this.map.tileToWorldY(pointerTileY);
+
+
+        if (this.input.manager.activePointer.isDown)
+        {
+            const tile = this.map.getTileAt(pointerTileX, pointerTileY);
+
+            if (tile)
+            {
+                // Note: JSON.stringify will convert the object tile properties to a string
+                this.propertiesText.setText(`Properties: ${JSON.stringify(tile.properties)}`);
+                tile.properties.viewed = true;
+            }
+
+            //  this.map.putTileAt(this.selectedTile, pointerTileX, pointerTileY);
+        }
+
         this.hero.update();
     }
 
