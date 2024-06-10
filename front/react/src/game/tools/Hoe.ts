0e7a9e71f34651ce488394ef1b81a39cc03814c4
vgenopoulos <vgenopoulos@IT3>
2024-06-10 12:16:45 +0300

    -

--- front/react/src/game/tools/Hoe.ts
+++ front/react/src/game/tools/Hoe.ts
@@ -6,7 +6,14 @@ export default class Hoe implements Tool, InventoryItem {
     public id:number;
     public isStackable:boolean;
     public amount: number
-    public cursorType:CursorType.HOE
+    public cursorType:CursorType
+
+    constructor() {
+        this.id = 1;
+        this.isStackable = false;
+        this.amount = 1;
+        this.cursorType = CursorType.HOE;
+    }
 
     public execute() {}
 }
