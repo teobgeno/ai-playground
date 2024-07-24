fa6a757446d17ff7af259d4b64025f643390f192
vgenopoulos <vgenopoulos@IT3>
2024-07-24 12:35:09 +0300

    -

--- start/api/router/conversation_handler.py
+++ start/api/router/conversation_handler.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, FastAPI, HTTPException
 from api.context import *
 import configparser
 from core.db.json_db_manager import JsonDBManager
@@ -6,7 +6,7 @@ from game.llm import LLMProvider
 from core.cache import Cache
 from schema.conversation import *
 from game.conversation_manager import ConversationManager
-
+from pydantic import ConfigDict, TypeAdapter, ValidationError
 
 router = APIRouter(
     prefix='/conversation'
@@ -21,5 +21,17 @@ async def conversation_talk(
     llm: LLMProvider = Depends(get_llm),
     cache: Cache = Depends(get_cache)
     ):
+
     conversation_manager = ConversationManager(parser, db, llm, cache, params)
     return conversation_manager.process_conversation()
+
+    # try:
+    #     ta = TypeAdapter(ConversationApiRequestDef)
+    #     ta.validate_python(params)
+    #     conversation_manager = ConversationManager(parser, db, llm, cache, params)
+    #     return conversation_manager.process_conversation()
+    # except ValidationError as e:
+    #     raise HTTPException(status_code=401, detail="Gia ton Poutso data")
+
+
+   
