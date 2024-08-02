44efec4148994d56cc977dc8b27236bbb7efb7ff
vgenopoulos <vgenopoulos@IT3>
2024-08-02 12:25:01 +0300

    -

--- front/react/src/game/items/ItemFactory.ts
+++ front/react/src/game/items/ItemFactory.ts
@@ -4,6 +4,8 @@ import { GenericItem } from "../items/GenericItem";
 import { CursorType } from "../cursors/types";
 import { ObjectId } from "../core/types";
 
+import { Seed } from "../farm/Seed";
+
 export class ItemFactory {
 
 
@@ -20,5 +22,23 @@ export class ItemFactory {
         );
 
         return fencePart;
+    }
+
+    public static createCornSeed(amount: number) {
+        
+        const seedCrop = new GenericItem(ObjectId.Corn,'Corn', new InventoryItem().setIcon('https://assets.codepen.io/7237686/corn.svg?format=auto'));
+        const cornSeed = new Seed(ObjectId.CornSeed, 'Corn Seeds', 
+            new InventoryItem()
+            .setIsStackable(true)
+            .setAmount(amount).setIcon('https://assets.codepen.io/7237686/poppy_seeds.svg?format=auto')
+            .setCursorType(CursorType.EXTERNAL_INTERACTION)
+        )
+        .setGrowthStageDuration(1000)
+        .setCurrentGrowthStageFrame(30)
+        .setStartGrowthStageFrame(30)
+        .setMaxGrowthStageFrame(34)
+        .setCrop(seedCrop);
+
+        return cornSeed;
     }
 }
