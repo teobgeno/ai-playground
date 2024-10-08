029be1d8f611884f2213aeb3d58c1565d7b5449d
vgenopoulos <vgenopoulos@IT3>
2024-10-08 15:30:52 +0300

    -

---
+++ front/react/src/game/cursors/TilesSelectCursor.ts
@@ -0,0 +1,89 @@
+import { Tilemaps } from "phaser";
+import { MapManager } from "../MapManager";
+import { GridEngine } from "grid-engine";
+
+import { Storable } from "../items/types";
+import { Character } from "../characters/types";
+import { Cursor } from "./types";
+import { MapObject } from "../core/types";
+
+export class TilesSelectCursor implements Cursor {
+    private scene: Phaser.Scene;
+    private map: Tilemaps.Tilemap;
+    private mapManager: MapManager;
+    private gridEngine: GridEngine;
+    private character: Character;
+    private marker: Phaser.GameObjects.Rectangle;
+    private canExecute: boolean = false;
+    private item: Storable;
+    private activeMarker: Phaser.GameObjects.Sprite | null;
+
+    public graphics: Phaser.GameObjects.Graphics;
+    public isDrawing: boolean = false;
+    public startPoint: { x: number, y: number } = { x: 0, y: 0 };
+    public currentRect: { x: number, y: number, width: number, height: number } = { x: 0, y: 0, width: 0, height: 0 };
+
+
+    constructor(
+        scene: Phaser.Scene,
+        map: Tilemaps.Tilemap,
+        mapManager: MapManager,
+        gridEngine: GridEngine,
+        character: Character,
+        marker: Phaser.GameObjects.Rectangle
+    ) {
+        this.scene = scene;
+        this.map = map;
+        this.mapManager = mapManager;
+        this.gridEngine = gridEngine;
+        this.character = character;
+        this.marker = marker;
+    }
+
+    public setCanExecute(canExecute: boolean) {
+       return false;
+    }
+
+    public hidePointer() {
+        this.activeMarker?.destroy();
+        this.activeMarker = null;
+        this.canExecute = false;
+    }
+
+    public onPointerMove(pointerTileX: number, pointerTileY: number) {
+
+        if (this.isDrawing) {
+            // const p = this.input.activePointer.positionToCamera(this.cameras.main)
+            // const width = (p as Phaser.Math.Vector2).x - this.startPoint.x;
+            // const height = (p as Phaser.Math.Vector2).y - this.startPoint.y;
+
+            // this.currentRect.x = this.startPoint.x;
+            // this.currentRect.y = this.startPoint.y;
+            // this.currentRect.width = width;
+            // this.currentRect.height = height;
+        }
+       
+    }
+
+    public onPointerUp(pointerTileX: number, pointerTileY: number) {
+        if(this.isDrawing) {
+            const elem : Array<MapObject> = [];
+            const inRec:Array<number> = [];
+            for (let xPos = this.currentRect.x; xPos < (this.currentRect.x + this.currentRect.width); xPos ++) {
+
+                for (let yPos = this.currentRect.y; yPos < (this.currentRect.y + this.currentRect.height); yPos ++) {
+                    const mapObj = this.mapManager.getPlotLandCoord(pointerTileX, pointerTileY); 
+                    if(mapObj !== null && mapObj !== undefined) {
+                        if(!inRec.includes(mapObj.id + mapObj.objectId)) {
+                            inRec.push(mapObj.id + mapObj.objectId);
+                            elem.push(mapObj);
+                        }
+                    }
+                }
+            }
+
+            console.log(elem)
+            this.isDrawing = false;
+        }
+    }
+}
